c833833ed95a4c1727e1b36cc1066f342fc8a288
choiyounji <yunji71@naver.com>
2023-05-09 13:21:20 +0900

    feat: change return message

--- src/main/java/capstone/avo/backend/controller/AvoController.java
+++ src/main/java/capstone/avo/backend/controller/AvoController.java
@@ -6,20 +6,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
+//import com.google.gson.JsonElement;
+//import com.google.gson.JsonObject;
+//import com.google.gson.JsonPrimitive;
 
 @RestController
 @RequestMapping("/status")
 @RequiredArgsConstructor
 public class AvoController {
-
     @GetMapping
     @ResponseStatus(value= HttpStatus.OK)
     public String returnStatus(){
-        JsonObject obj = new JsonObject();
-        obj.addProperty("status", "shouting");
-        return obj.toString();
+        //JsonObject obj = new JsonObject();
+        //obj.addProperty("status", "shouting");
+        //return obj.toString();
+        return "shouting";
     }
 }
