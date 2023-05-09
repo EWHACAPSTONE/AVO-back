c0ba8d6c91518c8f27cd449160d728b8b08f998f
choiyounji <100260416+choiyounji@users.noreply.github.com>
2023-05-09 18:37:03 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,17 +2,18 @@
 
 REPOSITORY=/home/ubuntu/app
 
-echo "> 현재 구동 중인 애플리케이션 port 확인"
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PORT=8080
+CURRENT_PID=$(pgrep -fla java | grep hayan | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl springboot-webservice | grep java | awk '{print $1}')
 
-echo "현재 구동 중인 애플리케이션 port: $CURRENT_PORT"
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
-if [ -z "$CURRENT_PORT" ]; then
+if [ -z "$CURRENT_PID" ]; then
   echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -9 $CURRENT_PORT"
-  kill -9 $CURRENT_PORT
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
   sleep 5
 fi
 
